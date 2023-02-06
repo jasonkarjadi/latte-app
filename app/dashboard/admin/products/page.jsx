1e44fb13a0f59e3d834b292539ba0f8e10ac7b42
Aivila <117703304+Aivila@users.noreply.github.com>
2023-02-06 19:28:40 +0700

    create products page (#4)

--- app/dashboard/admin/products/page.jsx
+++ app/dashboard/admin/products/page.jsx
@@ -1,8 +1,43 @@
+"use client";
+
+import { Flex, Grid, Spacer, Stack } from "@chakra-ui/react";
+import ProductCard from "./ProductCard";
+import BtnNav from "./BtnNav";
+import { useState } from "react";
+import Kategori from "./Kategori";
+
 const ProductsPage = () => {
+  const [isProducts, setIsProducts] = useState(false);
+
   return (
     <>
-      <h1>LATTE</h1>
-      <p>Hello World!</p>
+      <Flex direction={"column"}>
+        <Stack padding={10} spacing={"none"} direction="row" align={"center"}>
+          <BtnNav>Products</BtnNav>
+          <BtnNav>Categories</BtnNav>
+          <Spacer />
+          <BtnNav>Create +</BtnNav>
+        </Stack>
+        {isProducts ? (
+          <Grid templateColumns="repeat(3, 1fr)" gridGap={50} padding={"50px"}>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+          </Grid>
+        ) : (
+          <>
+            <Kategori></Kategori>
+          </>
+        )}
+      </Flex>
     </>
   );
 };
