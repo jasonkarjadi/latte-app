88615f000f47d2e6262a34c420e29608e6fe3e26
Aivila <you.aivila@gmail.com>
2023-02-07 15:51:16 +0700

    Create User page

--- app/dashboard/admin/users/page.jsx
+++ app/dashboard/admin/users/page.jsx
@@ -1,8 +1,32 @@
+"use client";
+
+import { Box, Table, TableContainer, Th, Thead, Tr } from "@chakra-ui/react";
+import RoleTable from "./RoleTable";
+
 const UsersPage = () => {
   return (
     <>
-      <h1>LATTE</h1>
-      <p>Hello World!</p>
+      <Box margin={("200", "50")}>
+        <TableContainer>
+          <Table variant={"striped"} colorScheme={"#85586F"}>
+            <Thead>
+              <Tr>
+                <Th>#</Th>
+                <Th>Name</Th>
+                <Th>Email</Th>
+                <Th>Phone No.</Th>
+                <Th>Password</Th>
+                <Th>Role</Th>
+              </Tr>
+            </Thead>
+            <RoleTable></RoleTable>
+            <RoleTable></RoleTable>
+            <RoleTable></RoleTable>
+            <RoleTable></RoleTable>
+            <RoleTable></RoleTable>
+          </Table>
+        </TableContainer>
+      </Box>
     </>
   );
 };
