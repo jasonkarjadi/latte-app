54f8a08200bd5b11d21f56ba584cc6c47dbe8d0d
jason@karjadi.com <jason@karjadi.com>
2023-02-09 15:28:10 +0700

    remove roboto font

--- fonts.js
+++ fonts.js
@@ -1,8 +1,5 @@
-import { Poppins, Roboto } from "@next/font/google";
+import { Poppins } from "@next/font/google";
 
 export const poppins = Poppins({
-  weight: ["400", "700"],
-});
-export const roboto = Roboto({
   weight: ["400", "700"],
 });
