9e21a094f32f369296c00f9f50e2da0a66a87b25
jason@karjadi.com <jason@karjadi.com>
2023-02-09 15:29:04 +0700

    simplify middleware

--- middleware.js
+++ middleware.js
@@ -1,19 +1,14 @@
 import { NextMiddleware, NextResponse } from "next/server";
-import { getAccessToken } from "./accessToken";
 
 /** @type {NextMiddleware} */
 
-export const middleware = (request, _event) => {
-  const { nextUrl, cookies, headers } = request;
+export const middleware = async (request, _event) => {
+  const { nextUrl, cookies } = request;
 
-  if (nextUrl.pathname.startsWith("/dashboard")) {
-    if (cookies.has("refreshToken")) {
-      const accessToken = getAccessToken();
-      if (accessToken) {
-        headers.set("authorization", `Bearer ${accessToken}`);
-      }
-    } else {
-      return NextResponse.redirect("http://localhost:3000");
-    }
+  if (
+    nextUrl.pathname.startsWith("/dashboard") &&
+    !cookies.has("refreshToken")
+  ) {
+    return NextResponse.redirect("http://localhost:3000");
   }
 };
