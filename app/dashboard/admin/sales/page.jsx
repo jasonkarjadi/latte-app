8b0fe24b67ce61b10857204b1e276ef2b4d82d98
jason@karjadi.com <jason@karjadi.com>
2023-02-17 17:15:30 +0700

    add data fetching

--- app/dashboard/admin/sales/page.jsx
+++ app/dashboard/admin/sales/page.jsx
@@ -1,10 +1,33 @@
 "use client";
 
 import { Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { getAccessToken, setAccessToken } from "../../../../accessToken";
 import CreateButton from "../CreateButton";
 import Sidebar from "../Sidebar";
 
 const SalesPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      setAccessToken(null);
+      const accessToken = await getAccessToken();
+      try {
+        const res = await fetch(`http://localhost:2000/transaction/list`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        const resBody = await res.json();
+        console.log(resBody);
+        setData(resBody);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Sidebar>
